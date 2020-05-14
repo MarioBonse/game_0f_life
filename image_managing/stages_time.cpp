6e7dcbc7e4932fd327b776d21b44c7c149d7eaf8
mariobonse <mario.bonsembiante@gmail.com>
2020-05-14 02:51:47 +0200

    Tested the time of the stages

--- image_managing/stages_time.cpp
+++ image_managing/stages_time.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <chrono>
 #include <string>
+#include <dirent.h>
 
 // keep in mind that all or some of these includes
 // may change depending on the library installation and version
@@ -20,43 +21,74 @@ bool is_number(const std::string& s)
         s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
 }
 
+using namespace cv;
 
 int main(int argc, char * argv[]) {
 
     // declare image containers 
     std::vector<std::string> images_name;
+    int scale = 1;
+    int delta = 0;
+    int ddepth = CV_16S;
     int i = 1;
-    while(!is_number(argv[i])){
-        images_name.push_back(argv[i]);
-        i++;
+    std::string path = argv[1];
+    if(path[path.length() - 1] != '/')
+        path = path + '/';
+    int n = path.length(); 
+    char path_char[n + 1]; 
+  
+    strcpy(path_char, path.c_str()); 
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(path_char) ) != NULL) {
+    /* print all the files and directories within directory */
+    while ((ent = readdir (dir)) != NULL) {
+        std::string ret(ent->d_name);
+        if(ret =="." || ret == "..")
+            continue;
+        images_name.push_back(ret);
+    }
+    closedir (dir);
+    } else {
+    /* could not open directory */
+    perror ("");
+    return EXIT_FAILURE;
     }
 
-    std::vector<cv::Mat> img(images_name.size()),dst(images_name.size());
+    std::vector<cv::Mat> imgages,dst;
+    cv::Mat src, result;
 
     // read one image from disk from disk
     auto start = std::chrono::high_resolution_clock::now();
-    img = cv::imread(images_name[0]);
+    for(auto img: images_name)
+        imgages.push_back(cv::imread(path + images_name[1]));   // Read the file
     auto elapsed = std::chrono::high_resolution_clock::now() - start;
     auto usec    = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
     std::cout <<"read took: "<< usec << std::endl;
 
     // apply first filter, producing a new image (just for example)
     start = std::chrono::high_resolution_clock::now();
-    cv::GaussianBlur(img, dst, cv::Size(3, 3), 0, 0, cv::BORDER_DEFAULT);
+    for(int i = 0; i<images_name.size();i++)
+        cv::GaussianBlur(imgages[i], imgages[i], cv::Size(3, 3), 0, 0, BORDER_DEFAULT);
     elapsed = std::chrono::high_resolution_clock::now() - start;
     usec    = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
     std::cout <<"Gaussian filter took: "<< usec << std::endl;
 
     // apply second filter. Here we overwrite the input (just for example)
     start = std::chrono::high_resolution_clock::now();
-    cv::Sobel(dst,dst,-1, 1, 1);
+    for(int i = 0; i<images_name.size();i++)
+        cv::Sobel( imgages[i],imgages[i], ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
     elapsed = std::chrono::high_resolution_clock::now() - start;
     usec    = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
     std::cout <<"Sobel filter took: "<< usec << std::endl;
 
     // write image to disk
     start = std::chrono::high_resolution_clock::now();
-    cv::imwrite("img/out"+images_name[0],dst);
+    for(int i = 0; i<images_name.size();i++){
+        //std::string name = "img/out" + images_name[i];
+        //std::cout<<name<<std::endl;
+        cv::imwrite("img/out" + images_name[i], imgages[i]);
+    }
     elapsed = std::chrono::high_resolution_clock::now() - start;
     usec    = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
     std::cout <<"write took: "<< usec << std::endl;
